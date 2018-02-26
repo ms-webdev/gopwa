2bed9490cee1b0365d788062ea6d761dab348fda
Marco Schwochow <ms.webdev@freenet.de>
2018-02-26 10:26:49 +0100

    homepage changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>goPWA - Progressive Web Apps Deutschland</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -77,19 +77,16 @@
                 </div>
             @endif
 
-            <div class="content">
+            <main class="content">
+                <h1>Progressive Web Apps</h1>
                 <div class="title m-b-md">
-                    Laravel
+                     goPWA - Deutschland
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div>
+
                 </div>
-            </div>
+            </main>
         </div>
     </body>
 </html>
